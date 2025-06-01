391b524a972a0d89b08f477cbbbc88de161a0cb7
Oleg Fostenko <oleg.fostenko.03@mail.ru>
2025-06-01 13:49:51 +0300

    Update and rename inf.py to inf_3d.py

--- est3d/further_research/inf_3d.py
+++ est3d/further_research/inf_3d.py
@@ -1,13 +1,15 @@
-# Get 2D keypoints from a video
+# Initialize 3D pose inferencer with MotionBERT (pretrained on Human3.6M)
+# Get a glimpse of the results achieved with MMPose's implementation
+
 from mmpose.apis import MMPoseInferencer
 
 # Initialize 3D pose inferencer
 inferencer = MMPoseInferencer(
-    pose2d='rtmpose-l_8xb512-700e_body8-halpe26-384x288'
+    pose3d='motionbert_dstformer-ft-243frm_8xb32-120e_h36m'
 )
 
 input_file_path = 'input_file.mp4'
-output_dir_path = 'output_dir'
+output_dir_path = 'output_file'
 
 # Run inference with automatic saving
 result_generator = inferencer(
@@ -15,7 +17,8 @@ result_generator = inferencer(
     show=True,
     out_dir=output_dir_path,
     return_vis=True,
-    save_pred=True  # <-- This tells it to save predictions automatically
+    save_pred=True,
+    disable_norm_pose_2d=False
 )
 
 while True:
@@ -26,3 +29,4 @@ while True:
         # If we reach the end of the generator, stop the loop
         print("No more results.")
         break  # should include 'keypoints', 'frame_id', etc.
+
