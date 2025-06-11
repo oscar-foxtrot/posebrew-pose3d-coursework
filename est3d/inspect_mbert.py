8bdeb6da2c0af55c044173eb2df1532af4df3017
Oleg Fostenko <oleg.fostenko.03@mail.ru>
2025-06-11 18:56:03 +0300

    Add files via upload

--- est3d/inspect_mbert.py
+++ est3d/inspect_mbert.py
@@ -13,9 +13,11 @@ print("Max values:", keypoints.max(axis=(0,1)))
 '''
 
 
+file_number = 795
+
 ######## NEW. GET KEYPOINTS
 from apply_kernel import get_kpts
-keypoints = get_kpts()
+keypoints = get_kpts(file_number)
 
 '''
 ### DELETE THIS
@@ -51,17 +53,19 @@ h36m_pts = [(3,2), (2,1), (1, 0), (0, 4), (4, 5), (5, 6), \
 scatter = ax.scatter([], [], [], color='blue')
 lines = [ax.plot([], [], [], color='blue')[0] for _ in h36m_pts]
 
-def get_ss(list_kpts_tuples, frame_kpts):
+def get_ss_weighted(list_kpts_tuples, frame_kpts, weights):
     ss = 0
     for i in range(len(list_kpts_tuples)):
         kpt2_coords = frame_kpts[list_kpts_tuples[i][1]]
         kpt1_coords = frame_kpts[list_kpts_tuples[i][0]]
-        ss += ((kpt2_coords[2] - kpt1_coords[2])**2 + (kpt2_coords[1] - kpt1_coords[1])**2 + (kpt2_coords[0] - kpt1_coords[0])**2)**0.5
+        ss += weights[i] * ((kpt2_coords[2] - kpt1_coords[2])**2 + (kpt2_coords[1] - kpt1_coords[1])**2 + (kpt2_coords[0] - kpt1_coords[0])**2)**0.5
     return ss
 
+n = len(h36m_pts)
+w0 = np.array([1 / (n)] * n)
 skeleton_sums = []
 for i in range(len(keypoints_3d)):
-    skeleton_sums += [get_ss(h36m_pts, keypoints_3d[i])]
+    skeleton_sums += [get_ss_weighted(h36m_pts, keypoints_3d[i], w0)]
 
 """
 print(skeleton_sums)
@@ -74,7 +78,10 @@ skeleton_sums = gaussian_filter1d(skeleton_sums, sigma=2)
 
 
 for i in range(len(keypoints_3d)):
-    #keypoints_3d[i] = (np.array(keypoints_3d[i]) / skeleton_sums[i]).tolist()
+
+    # Comment the lile below to remove normalization
+    
+    keypoints_3d[i] = (np.array(keypoints_3d[i]) / skeleton_sums[i]).tolist()
     for j in range(1, len(keypoints_3d[0])):
         keypoints_3d[i][j] = (np.array(keypoints_3d[i][j]) - np.array(keypoints_3d[i][0])).tolist()
     keypoints_3d[i][0] = [0, 0, 0]
@@ -88,9 +95,9 @@ ax.set_xlim([keypoints_3d[..., 0].min(), keypoints_3d[..., 0].max()])
 ax.set_ylim([keypoints_3d[..., 1].min(), keypoints_3d[..., 1].max()])
 ax.set_zlim([keypoints_3d[..., 2].min(), keypoints_3d[..., 2].max()])
 '''
-ax.set_xlim([-0.25, 0.25])
-ax.set_ylim([-0.25, 0.25])
-ax.set_zlim([-0.25, 0.25])
+ax.set_xlim([-2, 2])
+ax.set_ylim([-2, 2])
+ax.set_zlim([-2, 2])
 
 '''
 # Function to update the scatter plot for each frame
@@ -135,12 +142,13 @@ def update(frame):
     '''
     return [scatter] + lines
 
-# UNCOMMENT IF YOU WISH TO OBTAIN THE ENSEMBLED PREDICTIONS AS A FILE
+
 #np.save('file_795_2_withoutnorm.npy', np.array(keypoints_3d, dtype=object))
 # Animation
 
 # Create the animation
 ani = FuncAnimation(fig, update, frames=range(keypoints_3d.shape[0]), interval=8.33, blit=False)
-ani.save("animation.gif", writer="ffmpeg", fps=30)
+#ani.save("file_795_NONORM.gif", writer="ffmpeg", fps=30)
 # Show the plot
 plt.show()
+
